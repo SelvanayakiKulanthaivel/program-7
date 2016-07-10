b1e1da24baf7e606e2b0b28f92535196cc345bcb
SelvanayakiKulanthaivel <selviselvi2255.ss@gmail.com>
2016-07-10 12:59:34 +0530

    Create myprogram.c

---
+++ myprogram.c
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int x=(input("Enter x "));
+int y=int(input("Enter y "));
+res=int(x);
+s=list(x);
+for i in s
+	res*=int(i)
+if(res==y)
+	print (str(x)+"is seed of " + str(y));
+else:
+	print (str(x)+"is not seed of ",y);
+	getch();
+	}
+
+
